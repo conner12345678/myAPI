3a87f341b90d28e7ca144bd5b13b77869d4f372f
conner12345678 <cehly065@west-mec.org>
2024-10-02 14:38:29 -0700

    stuff

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const {movies, actors} = require('./db/movies.js')
+// const {movies, actors} = require('./db/movies.js')
 const path = require('path')
 const fs = require('fs')
 const bodyParser = require('body-parser')
@@ -10,25 +10,39 @@ app.set('view engine', 'ejs')
 
 app.use(express.static(path.join(__dirname, '/public')))
 
+const getMovies = () => {
+    const data = fs.readFileSync('./db/movies.json', 'utf8')
+    return JSON.parse(data)
+}
+
+const getActors = () => {
+    const data = fs.readFileSync('./db/actors.json', 'utf8')
+    return JSON.parse(data)
+}
+
+const saveMovies = (movies) => {
+    fs.writeFileSync('./db/movies.json', JSON.stringify(movies, null, 2), 'utf8')
+}
+
+const saveActors = (actors) => {
+    fs.writeFileSync('./db/actors.json', JSON.stringify(actors, null, 2), 'utf8')
+}
+
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 })
 
 app.get('/api/movies', (req,res) => {
-    const theMovies = movies.map(theMovie => {
-        const {movie, yearCreated} = theMovie
-        return {movie, yearCreated}
-    })
+    const theMovies = getMovies()
     res.json(theMovies)
 })
 
 app.get('/api/:id/movie', (req,res) => {
-    const {id} = req.params
-    const singleMovie = movies.find((movie) => movie.id === Number(id))
-    if(!singleMovie){
-        return res.status(404).send("Movie does not exist, and might never")
-    }
-    return res.json(singleMovie)
+    const movies = getMovies()
+    const singleMovie = movies.findIndex(movie => movie.id == req.params.id)
+    const theMovie = movies.find((movie) => movie.id == Number(singleMovie+1))
+    console.log(theMovie)
+    return res.json(theMovie)
 })
 
 app.get('/form', (req,res) => {
@@ -36,13 +50,24 @@ app.get('/form', (req,res) => {
 })
 
 app.post('/form/redir', (req,res) => {
-    const {movie, yearCreated} = req.body
+    const movies = getMovies()
+    const actors = getActors()
+    const {movie, yearCreated, actor, birthDate} = req.body
     const newMovie =  {
         id: movies.length + 1,
         movie: movie,
         yearCreated: yearCreated
     }
+    const newActor = {
+        actor_id: actors.length+1,
+        actor: actor,
+        birthDate: birthDate,
+        comment: "this is the new comments"
+    }
     movies.push(newMovie)
+    saveMovies(movies)
+    actors.push(newActor)
+    saveActors(actors)
     res.redirect('/')
 })
 
@@ -50,15 +75,16 @@ app.get('/delete', (req,res) => {
     res.sendFile(path.join(__dirname, './public/delete.html'))
 })
 
-app.delete('/delete/redir', (req,res) => {
-    const id = req.body
-    const index = movies.findIndex(movie => movie.id === id)
-    if(!id){
-        res.status(404).send('The Id was not found')
-    }
-    movies.splice(index, 1)
-    res.redirect('/')
-})
+// app.delete('/delete/redir', (req,res) => {
+//     const id = req.params
+//     console.log(id)
+//     const index = movies.findIndex(movie => movie.id === id)
+//     if(!id){
+//         res.status(404).send('The Id was not found')
+//     }
+//     movies.splice(index, 1)
+//     res.redirect('/')
+// })
 
 app.listen(5000, () => {
     console.log('Server running on port 5000')
